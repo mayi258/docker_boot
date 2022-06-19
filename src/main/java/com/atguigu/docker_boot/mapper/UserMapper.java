967d751430c965ff69cee4f5886ef294ec43b7ff
Alice <762902158@qq.com>
2022-06-19 08:54:00 +0800

    Docker Demo first commit

---
+++ src/main/java/com/atguigu/docker_boot/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.atguigu.docker_boot.mapper;
+
+import com.atguigu.docker_boot.entity.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
---
+++ src/test/java/com/atguigu/docker_boot/DockerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.docker_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
